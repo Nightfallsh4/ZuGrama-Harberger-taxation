19aa59e0441b05f4d34146647ee0aa583921334d
Shanmugadevan <shanmugadevan@protonmail.com>
2024-10-28 20:49:39 +0530

    Made SBT only transferrable by Harberger

--- src/SBT.sol
+++ src/SBT.sol
@@ -2,7 +2,31 @@
 pragma solidity 0.8.26;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "src/utils/Errors.sol";
 
 contract SBT is ERC721 {
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) { }
+    address public immutable harberger;
+
+    modifier onlyHarberger() {
+        if (msg.sender != harberger) {
+            revert SBT_Only_Harberger();
+        }
+        _;
+    }
+
+    constructor(string memory _name, string memory _symbol, address _harberger) ERC721(_name, _symbol) {
+        harberger = _harberger;
+    }
+
+    function approve(address to, uint256 tokenId) public override onlyHarberger {
+        _approve(to, tokenId, _msgSender());
+    }
+
+    function setApprovalForAll(address operator, bool approved) public override {
+        revert SBT_Function_Disabled();
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyHarberger {
+        super.transferFrom(from, to, tokenId);
+    }
 }
---
+++ src/utils/Errors.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+error SBT_Function_Disabled();
+error SBT_Only_Harberger();
